73e45c0a984f4a2bdc2816447e0cc2d4a72d7868
Nicolly <80932261+nicollycrs@users.noreply.github.com>
2024-09-26 17:49:09 -0300

    Create mini_max_sum.py

---
+++ preparation-kit-1-week/mini_max_sum.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    """Finds the minimum and maximum values that can be calculated by summing exactly four of the five integers of an array.
+    
+    Parameters
+    ----------
+    arr: INTEGER_ARRAY
+        An array of 5 integers.
+    """
+
+    # Creates a list to save sum results.
+    miniMaxSumArr = []
+
+    # Iterates the array summing every element, except for the current index.
+    for i, _ in enumerate(arr):
+        aux = arr.copy()
+        aux.pop(i)
+        miniMaxSumArr.append(sum(aux))
+        
+    # Gets the min and maximum values from the sum results.
+    minVal = min(miniMaxSumArr)
+    maxVal = max(miniMaxSumArr)
+
+    # Prints the results
+    print(f"{str(minVal)} {str(maxVal)}")
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
