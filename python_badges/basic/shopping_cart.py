b6db9961a5f6130d74f6758edb12aea1497df8b4
Nicolly <80932261+nicollycrs@users.noreply.github.com>
2024-09-29 21:26:05 -0300

    Update shopping_cart.py docstrings

--- python_badges/basic/shopping_cart.py
+++ python_badges/basic/shopping_cart.py
@@ -8,24 +8,48 @@ import sys
 
 
 class Item():
-  """ """
+  """Class that initializes an item."""
     def __init__(self, name, price):
+      """Initializes the item's name and price variables"""
         self.name = name
         self.price = price
 
 class ShoppingCart:
+  """Class that manages a shopping cart by adding items and returning the total price."""
     def __init__(self):
+      """Initializes the cart and total price variables."""
         self.cart = []
         self.price_total = 0
     
     def __len__(self):
+      """Returns the number of items added in the cart.
+      
+      Returns
+      -------
+      len(self.cart): int
+          The number of items in the cart.
+      """
         return len(self.cart)
     
     def add(self, item: Item):
+      """Adds an item to the shopping cart.
+
+      Parameters
+      ----------
+      item: Item
+          The item to be added to the cart.
+      """
         self.cart.append(item.name)
         self.price_total += item.price
     
     def total(self):
+      """Returns the shopping cart's total price.
+
+      Returns
+      -------
+      self.price_total: float
+          The shopping cart's total price.
+      """
         return self.price_total
         
 if __name__ == '__main__':
